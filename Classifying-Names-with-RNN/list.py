e3d7ea205b1ac383a0a8ca9452565766daed9cae
ZengyuanYu <1952284157@qq.com>
2018-02-17 14:31:11 +0800

    列表和元组复习

---
+++ Classifying-Names-with-RNN/list.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# 创建一个列表
+classmate = ['he', 'she', 'it']
+print(classmate)
+# 计算列表长度
+print(len(classmate))
+# [-1]输出列表最后一个元素
+print(classmate[1], classmate[-1])
+# 添加元素
+classmate.append('his')
+print(classmate)
