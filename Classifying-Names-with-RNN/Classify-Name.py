3aabbf06c8501a72864c53046def5fb732e6193e
ZengyuanYu <1952284157@qq.com>
2018-02-18 10:15:56 +0800

    添加评估和绘制误差曲线

--- Classifying-Names-with-RNN/Classify-Name.py
+++ Classifying-Names-with-RNN/Classify-Name.py
@@ -171,6 +171,9 @@ n_iters = 100000
 print_every = 5000
 plot_every = 1000
 
+
+
+# Keep track of losses for plotting
 current_loss = 0
 all_losses = []
 
@@ -188,12 +191,88 @@ for iter in range(1, n_iters + 1):
     output, loss = train(category_tensor, line_tensor)
     current_loss += loss
 
+    # Print iter number, loss, name and guess
     if iter % print_every == 0:
         guess, guess_i = categoryFromOutput(output)
         correct = '✓' if guess == category else '✗ (%s)' % category
-        print(
-            '%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
 
+    # Add current loss avg to list of losses
     if iter % plot_every == 0:
         all_losses.append(current_loss / plot_every)
         current_loss = 0
+
+#********************************************************************#
+#可视化
+#********************************************************************#
+print('开始可视化')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+# Go through a bunch of examples and record which are correctly guessed
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+# Normalize by dividing every row by its sum
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# Set up plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# Set up axes
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+# Force label at every tick
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# sphinx_gallery_thumbnail_number = 2
+plt.show()
+
+######################################################################
+# Running on User Input
+# ---------------------
+#
+
+def predict(input_line, n_predictions=10):
+    print('\n> %s' % input_line)
+    output = evaluate(Variable(lineToTensor(input_line)))
+
+    # Get top N categories
+    topv, topi = output.data.topk(n_predictions, 1, True)
+    predictions = []
+
+    for i in range(n_predictions):
+        value = topv[0][i]
+        category_index = topi[0][i]
+        print('(%.2f) %s' % (value, all_categories[category_index]))
+        predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
+
